d6f9e29eeeec35aaa46f2db679eedf9370842f8f
kstachowicz <k.stachowicz@hotmail.com>
2020-05-10 23:52:30 +0200

    Update index.js

--- env-req-parser/index.js
+++ env-req-parser/index.js
@@ -9,18 +9,9 @@ try {
     if(!issue.body) return;
     if(!issue.labels) return;
 
-    //let labels = Array.from(issue.labels);
-    //let approved = labels.some(val =>name == "approved");
-
-    // if(!approved) {
-    //     console.log("Issue is not approved.");
-    //     core.setOutput("approved", 'false');
-    //     return;
-    // }
-
     console.log("Issue is approved correctly.")
 
-    let appName = "", armTemplate = "", applyPolicy = false, policyName = "";
+    let appName = "", armTemplate = "", applyPolicy = false, policyName = "", policyDefinition = "";
 
     console.log(issue.body);
     const lines = issue.body.match(/[^\r\n]+/g)
@@ -37,11 +28,13 @@ try {
             armTemplate = "function-app";
         if (lines[i].startsWith("- [x] PCI")) {
             applyPolicy = true;
-            policyName = "Audit PCI";
+            policyName = "Audit PCI";       
+            policyDefinition = "[Preview]: Audit PCI v3.2.1:2018 controls and deploy specific VM Extensions to support audit requirements";
         }
         if (lines[i].startsWith("- [x] HIPAA")) {
             applyPolicy = true;
             policyName = "Audit HITRUST/HIPAA";
+            policyDefinition = "Audit HITRUST/HIPAA controls and deploy specific VM Extensions to support audit requirements";
         }
     }
 
@@ -50,12 +43,14 @@ try {
     core.setOutput('approved', 'true');
     core.setOutput('applyPolicy', applyPolicy ? 'true' : 'false');
     core.setOutput('policyName', policyName);
+     core.setOutput('policyDefinition', policyDefinition);
 
     console.log("appName: ", appName);
     console.log('armTemplate: ', armTemplate);
     console.log('approved: ', 'true');
     console.log('applyPolicy: ', applyPolicy ? 'true' : 'false');
     console.log('policyName: ', policyName);
+    console.log('policyDefinition: ', policyDefinition);
 
 } catch(error) {
     core.setFailed(error.message);
